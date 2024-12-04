d03bd3bef64ea1f5cb9dea2d98fd09cfc3ad0da3
NastyaTailakova <101914426+NastyaTailakova@users.noreply.github.com>
2024-12-04 19:51:15 +0700

    Add files via upload

---
+++ lr6/lr6/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace lr6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Gold;
+            this.StartButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.StartButton.Location = new System.Drawing.Point(339, 352);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(104, 40);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Старт";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(30, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Location = new System.Drawing.Point(286, 175);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Location = new System.Drawing.Point(545, 42);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ lr6/lr6/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lr6
+{
+    public partial class Form1 : Form
+    {
+        private Thread thread1;
+        private Thread thread2;
+        private Thread thread3;
+
+        private bool isRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePanels();
+        }
+
+        private void InitializePanels()
+        {
+            panel1.BackColor = Color.Red;
+            panel2.BackColor = Color.Green;
+            panel3.BackColor = Color.Blue;
+
+            panel1.Size = new Size(50, 50);
+            panel2.Size = new Size(50, 50);
+            panel3.Size = new Size(50, 50);
+
+            panel1.Location = new Point(10, 10);
+            panel2.Location = new Point(10, 100);
+            panel3.Location = new Point(10, 190);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                thread1 = new Thread(() => MovePanel(panel1));
+                thread2 = new Thread(() => MovePanel(panel2));
+                thread3 = new Thread(() => MovePanel(panel3));
+
+                thread1.Start();
+                thread2.Start();
+                thread3.Start();
+            }
+        }
+
+        private void MovePanel(Panel panel)
+        {
+            Random random = new Random();
+            int direction = random.Next(1, 3); // 1 - вправо, 2 - влево
+            int moveAmount = 10;
+
+            while (isRunning)
+            {
+                if (direction == 1) // двигаемся вправо
+                {
+                    panel.Invoke((MethodInvoker)delegate
+                    {
+                        if (panel.Location.X < this.Width - panel.Width)
+                        {
+                            panel.Location = new Point(panel.Location.X + moveAmount, panel.Location.Y);
+                        }
+                        else
+                        {
+                            direction = 2; // меняем направление
+                        }
+                    });
+                }
+                else // двигаемся влево
+                {
+                    panel.Invoke((MethodInvoker)delegate
+                    {
+                        if (panel.Location.X > 0)
+                        {
+                            panel.Location = new Point(panel.Location.X - moveAmount, panel.Location.Y);
+                        }
+                        else
+                        {
+                            direction = 1; // меняем направление
+                        }
+                    });
+                }
+
+                Thread.Sleep(100); // Задержка для контролируемого движения
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            isRunning = false; // Прекращаем движение при закрытии формы
+            thread1?.Join();
+            thread2?.Join();
+            thread3?.Join();
+            base.OnFormClosing(e);
+        }
+    }
+}
+
